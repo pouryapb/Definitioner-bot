39b584c90911e167ce90f3c6b8887783099865e3
Pourya Pourbagheri <34511048+pouryapb@users.noreply.github.com>
2021-02-01 15:43:01 +0330

    webhook 5

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 // Set telegram webhook
 // The second argument is necessary only if the client uses a self-signed
 // certificate. Including it for a verified certificate may cause things to break.
-bot.telegram.setWebhook(process.env.URL);
+bot.telegram.setWebhook(process.env.URL + "secret-path");
 
 // Http webhook, for nginx/heroku users.
 bot.startWebhook("/secret-path", null, process.env.$PORT);
