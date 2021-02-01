a275a53c915bc67081aaeb05a5c1e7c76d2ee360
Pourya Pourbagheri <34511048+pouryapb@users.noreply.github.com>
2021-02-01 19:18:51 +0330

    fuck me

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ bot.on("inline_query", (ctx) => {
       return res.json();
     })
     .then((resBody) => {
-      if (resBody !== null) {
+      if (resBody === null) {
         ctx.answerInlineQuery([]);
       } else {
         const defs = resBody;
